cd1346c6c46398b44cc83f4a2dc50c9c4967416f
kobiliusz <kobiliusz@gmail.com>
2024-02-25 20:06:28 +0100

    some translations

--- src/main.js
+++ src/main.js
@@ -149,6 +149,126 @@ const messages = {
             close: 'schließen'
         }
     },
+
+    cs: {
+        message: {
+            instructions: 'Tato stránka vám umožňuje vytvářet memy na základě vašich obrázků! Jednoduše nahrajte obrázek, nastavte horní a dolní text a stáhněte vytvořený soubor...',
+            top: 'Vrchní text',
+            bottom: 'Spodní text',
+            download: 'Stažení',
+            clear: 'vyčistit',
+            input: 'Přetáhněte soubor nebo kliknutím nahrajte obrázek!',
+            error: 'Chyba',
+            message: 'S obrázkem je něco špatně.',
+            close: 'Zavřít'
+        }
+    },
+    sk: {
+        message: {
+            instructions: 'Táto stránka vám umožňuje vytvárať memy na základe vašich obrázkov! Jednoducho nahrajte obrázok, nastavte horný a spodný text a stiahnite vytvorený súbor...',
+            top: 'Vrchný text',
+            bottom: 'Spodný text',
+            download: 'Stiahnuť',
+            clear: 'vyčistiť',
+            input: 'Presuňte súbor alebo kliknutím nahrajte obrázok!',
+            error: 'Chyba',
+            message: 'S obrázkom niečo nie je v poriadku.',
+            close: 'Zavrieť'
+        }
+    },
+
+    hu: {
+        message: {
+            instructions: 'Ezen az oldalon mémeket hozhatsz létre a képeid alapján! Egyszerűen töltse fel a képet, állítsa be a felső és alsó szöveget, és töltse le a létrehozott fájlt...',
+            top: 'Felső szöveg',
+            bottom: 'Alsó szöveg',
+            download: 'Letöltés',
+            clear: 'tisztázni',
+            input: 'Dobd el a fájlt vagy kattints a kép feltöltéséhez!',
+            error: 'Hiba',
+            message: 'Valami baj van a képpel.',
+            close: 'Bezárás'
+        }
+    },
+    ro: {
+        message: {
+            instructions: 'Acest site vă permite să creați meme-uri pe baza imaginilor dvs.! Pur și simplu încărcați imaginea, setați textul de sus și de jos și descărcați fișierul creat...',
+            top: 'Textul de sus',
+            bottom: 'Textul de jos',
+            download: 'Descarca',
+            clear: 'șterge',
+            input: 'Aruncați fișierul sau faceți clic pentru a încărca imaginea!',
+            error: 'Eroare',
+            message: 'E ceva în neregulă cu imaginea.',
+            close: 'Închide'
+        }
+    },
+
+    fi: {
+        message: {
+            instructions: 'Tämän sivuston avulla voit luoda meemejä kuviesi perusteella! Lataa vain kuva, aseta ylä- ja alateksti ja lataa luotu tiedosto...',
+            top: 'Ylin teksti',
+            bottom: 'Pohja teksti',
+            download: 'ladata',
+            clear: 'tyhjentää',
+            input: 'Pudota tiedosto tai lataa kuva napsauttamalla!',
+            error: 'Virhe',
+            message: 'Kuvassa on jotain vikaa.',
+            close: 'kiinni'
+        }
+    },
+    sv: {
+        message: {
+            instructions: 'Denna sida låter dig skapa memes baserat på dina bilder! Ladda bara upp bilden, ställ in den övre och nedre texten och ladda ner den skapade filen...',
+            top: 'Översta text',
+            bottom: 'Nedre text',
+            download: 'Ladda ner',
+            clear: 'rensa',
+            input: 'Släpp filen eller klicka för att ladda upp bilden!',
+            error: 'Fel',
+            message: 'Det är något fel på bilden.',
+            close: 'Stänga'
+        }
+    },
+    no: {
+        message: {
+            instructions: 'Denne siden lar deg lage memes basert på bildene dine! Bare last opp bildet, sett den øverste og nederste teksten og last ned den opprettede filen...',
+            top: 'Topp tekst',
+            bottom: 'Nederste tekst',
+            download: 'nedlasting',
+            clear: 'fjerne',
+            input: 'Slipp fil eller klikk for å laste opp bilde!',
+            error: 'Feil',
+            message: 'Det er noe galt med bildet.',
+            close: 'Lukk'
+        }
+    },
+    da: {
+        message: {
+            instructions: 'Dette websted giver dig mulighed for at oprette memes baseret på dine billeder! Du skal blot uploade billedet, indstille den øverste og nederste tekst og downloade den oprettede fil...',
+            top: 'Toptekst',
+            bottom: 'Nederste tekst',
+            download: 'Hent',
+            clear: 'fjerne',
+            input: 'Drop fil eller klik for at uploade billede!',
+            error: 'Fejl',
+            message: 'Der er noget galt med billedet.',
+            close: 'Tæt'
+        }
+    },
+    is: {
+        message: {
+            instructions: 'Þessi síða gerir þér kleift að búa til memes byggt á myndunum þínum! Hladdu einfaldlega inn myndinni, stilltu efsta og neðsta textann og halaðu niður skránni sem búin var til...',
+            top: 'Efsti texti',
+            bottom: 'Neðri texti',
+            download: 'Sækja',
+            clear: 'hreinsa',
+            input: 'Slepptu skránni eða smelltu til að hlaða upp mynd!',
+            error: 'Villa',
+            message: 'Það er eitthvað athugavert við myndina.',
+            close: 'Loka'
+        }
+    },
 };
 
 const browserLanguage = navigator.language.split('-')[0];
